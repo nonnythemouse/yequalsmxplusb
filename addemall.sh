33feab576e0e4fab3fb28334a64f49833cb828fd
tmrhombus <tomperry7@gmail.com>
2017-09-16 11:22:06 +0200

    compile first

--- addemall.sh
+++ addemall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+pushd book1; pdflatex book1.tex; pdflatex book1.tex; pdflatex book1.tex; popd > /dev/null;
+pushd ebook; pdflatex ebook.tex; pdflatex ebook.tex; pdflatex ebook.tex; popd > /dev/null;
+pushd raw; pdflatex raw.tex; pdflatex raw.tex; pdflatex raw.tex; popd > /dev/null;
+
 git add book1/book1.pdf
 git add ebook/ebook.pdf
 git add raw/raw.pdf
